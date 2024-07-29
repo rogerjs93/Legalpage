4c73db5332e81567566923e532cbc700a5f80229
Roger Salas <58913883+rogerjs93@users.noreply.github.com>
2024-07-29 16:55:35 +0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+// tailwind.config.js
+module.exports = {
+  darkMode: 'class', // This enables dark mode
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
